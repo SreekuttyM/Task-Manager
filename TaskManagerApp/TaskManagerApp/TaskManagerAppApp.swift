35dbda3b20baf1702495519e6bcf5351076f6667
Sreekutty Maya <sreekuttystar@gmail.com>
2025-02-25 19:05:06 +0400

    Project SetUp Added

---
+++ TaskManagerApp/TaskManagerApp/TaskManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerAppApp.swift
+//  TaskManagerApp
+//
+//  Created by Sreekutty Maya on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
